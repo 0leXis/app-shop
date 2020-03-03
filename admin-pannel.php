6e6a8da6836fff90a5fc2404c80da2f5b6d20bc5
0leXis <alexbashur@gmail.com>
2020-03-03 19:46:00 +0300

    Admin back-end

---
+++ admin-pannel.php
@@ -0,0 +1,16 @@
+<?php
+    $TITLE = "Главная";
+    include("layout_admintop.php");
+?>
+        <div class="content">
+            <div class="admin-main-header">
+                <h2>Добро пожаловать!</h2>
+                <p>Выберите нужную вкладку в левом меню</p>
+            </div>
+        </div>
+    </div>
+    <?php
+        include("layout_footer.php");
+    ?>
+</body>
+</html>
---
+++ layout_admintop.php
@@ -0,0 +1,70 @@
+<?php
+    if(!isset($_SESSION))
+        session_start();
+    if(!isset($_SESSION['user_isadmin']))
+        header("Location: ErrorPages/401.html");
+    else
+    if($_SESSION['user_isadmin'] != true)
+        header("Location: ErrorPages/403.html");
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title><?= "OFFLINER ADMIN - " . $TITLE?></title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/style.css" type="text/css" />
+</head>
+
+<body>
+    <header>
+        <div class="header-login">
+            <div class="centered-container">
+                <?php
+                    if(isset($_SESSION['user_id'])){
+                        echo "<span>" . $_SESSION['user_email'] . "</span>";
+                        echo "<span>|</span>";
+                        if($_SESSION['user_isadmin']){
+                            echo '<a href="admin-pannel.php">Панель администратора</a>';
+                        }
+                        else{
+                            echo '<a href="orders.php">Мои заказы</a>';
+                        }
+                        echo "<span>|</span>";
+                        echo '<a href="modules/logout.php">Выйти из аккаунта</a>';
+                    }
+                    else{
+                        echo '<a href="login.php">Вход/Регистрация</a>';
+                    }
+                ?>
+            </div>
+        </div>
+        <div class="header-main">
+            <div class="centered-container">
+                <div class="header-logo">
+                    <image src="images/Logo.png" alt="Logo" />
+                    <span class="big-txt">Offliner</span>
+                </div>
+            </div>
+        </div>
+        <div class="header-blueline">
+        </div>
+    </header>
+    <div class="admin-main">
+        <aside>
+            <nav>
+                <ul>
+                    <li><a href="admin-pannel-products.php">Товары</a></li>
+                    <li><a href="admin-pannel-staff.php">Сотрудники</a></li>
+                    <li><a href="admin-pannel-orders.php">Заказы</a></li>
+                    <li><a href="admin-pannel-warehouses.php">Склады</a></li>
+                    <li><a href="admin-pannel-suppliers.php">Поставщики</a></li>
+                    <li><a href="admin-pannel-supply.php">Поставки</a></li>
+                    <li><a href="admin-pannel-manufacturers.php">Производители</a></li>
+                    <li><a href="admin-pannel-additional-info.php">Редактирование дополнительной информации</a></li>
+                    <li><a href="admin-pannel-messages.php">Сообщения</a></li>
+                    <li><a href="admin-pannel-log.php">Просмотр Log</a></li>
+                </ul>
+            </nav>
+        </aside>
